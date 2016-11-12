03ebd8955fb50fb2e20340f216861fae9fd621ec
Felix Jacobi <felix.jacobi@stsbl.de>
2016-11-12 21:27:01 +0100

    win10.reg: Fixed registry value name.

--- iconf/etc/samba/netlogon/win10.reg/80winst-microsoft-windows-10-64-upgrade.sh
+++ iconf/etc/samba/netlogon/win10.reg/80winst-microsoft-windows-10-64-upgrade.sh
@@ -6,8 +6,8 @@ echo "[HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate]"
 if [ "$HoldBackWin10Upgrades" = "1" ]
 then
   echo "; Windows 10 Upgrades drei Monate zurückhalten"
-  echo '"DeferUpgrades"=dword:1'
+  echo '"DeferUpgrade"=dword:1'
 else
   echo "; Zurückhaltungseinstellungen zurücksetzen"
-  echo '"DeferUpgrades"=-'
+  echo '"DeferUpgrade"=-'
 fi
