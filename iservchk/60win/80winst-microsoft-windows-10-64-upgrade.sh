f7ded962b6ef68e86dd584da0059fc4b14e65105
Felix Jacobi <felix.jacobi@iserv.eu>
2018-09-23 15:56:02 +0200

    Fixed iservchk script

--- iservchk/60win/80winst-microsoft-windows-10-64-upgrade.sh
+++ iservchk/60win/80winst-microsoft-windows-10-64-upgrade.sh
@@ -8,19 +8,19 @@ then
 # apply the upgrade package to all computers that have Windows 10 x64 applied
 Test "Apply Microsoft Windows 10 Upgrade x64 to all Windows 10 x64 computers"
   "! psql -Atc \"
-    SELECT host FROM deploy_state
+    SELECT host_id FROM deploy_state
     WHERE product = 'microsoft-windows-10-64'
-      AND host NOT IN
-        (SELECT host FROM deploy_state
+      AND host_id NOT IN
+        (SELECT host_id FROM deploy_state
         WHERE product = 'microsoft-windows-10-64-upgrade')
     \" iserv postgres | grep -q ."
   "psql -c \"
-    INSERT INTO deploy_state (host, product, action)
-    SELECT host, 'microsoft-windows-10-64-upgrade', 'setup'
+    INSERT INTO deploy_state (host, host_id, product, action)
+    SELECT host, host_id, 'microsoft-windows-10-64-upgrade', 'setup'
     FROM deploy_state
     WHERE product = 'microsoft-windows-10-64'
-      AND host NOT IN
-        (SELECT host FROM deploy_state
+      AND host_id NOT IN
+        (SELECT host_id FROM deploy_state
         WHERE product = 'microsoft-windows-10-64-upgrade');
     \" iserv postgres"
 
